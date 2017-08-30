dc57e94a266fa4c50e88d73a35d635a95458076d
DIYgod <diy.d.god@gmail.com>
2017-08-31 02:14:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# DPlayer-thumbnails
+
+> Generate thumbnails for [DPlayer](https://github.com/MoePlayer/DPlayer)
+
+[![npm](https://img.shields.io/npm/v/dplayer-thumbnails.svg?style=flat-square)](https://www.npmjs.com/package/dplayer-thumbnails)
+[![npm](https://img.shields.io/npm/dt/dplayer-thumbnails.svg?style=flat-square)](https://www.npmjs.com/package/dplayer-thumbnails)
+[![devDependency Status](https://img.shields.io/david/dev/MoePlayer/DPlayer-thumbnails.svg?style=flat-square)](https://david-dm.org/MoePlayer/DPlayer-thumbnails#info=devDependencies)
+
+## Install
+
+```
+$ npm install -g dplayer-thumbnails
+```
+
+## Usage
+```
+$ dplayer-thumbnails --help
+$ dplayer-thumbnails -o ./thumbnails.jpg -q 60 demo.mp4
+```
+
+## Author
+
+**DPlayer-thumbnails** © [DIYgod](https://github.com/DIYgod), Released under the [MIT](./LICENSE) License.<br>
+Authored and maintained by DIYgod with help from contributors ([list](https://github.com/DIYgod/DPlayer-thumbnails/contributors)).
+
+> Blog [@Anotherhome](https://www.anotherhome.net) · GitHub [@DIYgod](https://github.com/DIYgod) · Twitter [@DIYgod](https://twitter.com/DIYgod) · Telegram Channel [@awesomeDIYgod](https://t.me/awesomeDIYgod)
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+var ffmpeg = require('fluent-ffmpeg');
+var nsg = require('node-sprite-generator');
+var Jimp = require('jimp');
+var rm = require('rimraf');
+var Spinner = require('cli-spinner').Spinner;
+var program = require('commander');
+var os = require('os');
+var spinner;
+
+function startSpinner (string, step) {
+    spinner = new Spinner(`[${step}/3] %s ${string}...`);
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
+}
+
+var startTime = +new Date();
+var tmp = os.tmpdir() + '/dplayer-thumbnails'
+
+program
+    .version('0.0.1')
+    .usage('[options] <file>')
+    .option('-o, --output <path>', 'thumbnails path, default: ./thumbnails.jpg', './thumbnails.jpg')
+    .option('-q, --quality <n>', 'thumbnails quality, default: 60', 60)
+    .description('🎉  Generate thumbnails for DPlayer')
+    .action(function (file) {
+        startSpinner('Screenshots generating', 1);
+        ffmpeg(file)
+            .screenshots({
+                count: 100,
+                folder: tmp,
+                filename: 'screenshot%i.png',
+                size: '160x?'
+            })
+            .on('end', function () {
+                spinner.stop(true);
+                console.log('[1/3] Screenshots generated!');
+                startSpinner('Sprite generating', 2);
+
+                nsg({
+                    src: [
+                        tmp + '/*.png'
+                    ],
+                    spritePath: tmp + '/sprite.png',
+                    stylesheetPath: tmp + '/sprite.css'
+                }, function (err) {
+                    spinner.stop(true);
+                    console.log('[2/3] Sprite generated!');
+                    startSpinner('Compressing', 3);
+
+                    Jimp.read(tmp + '/sprite.png', function (err, lenna) {
+                        if (err) throw err;
+                        lenna.quality(parseInt(program.quality))
+                            .write(program.output);
+                        rm(tmp, function () {
+                            spinner.stop(true);
+                            console.log('[3/3] Compressing complete!');
+                            console.log(`✨  Done in ${(+new Date() - startTime) / 1000}s.`);
+                        });
+                    });
+                });
+            });
+    });
+
+program.parse(process.argv);
